940ab494264d816e1ae6582d765e6711abd671a4
Niansong Zhang <niansongz@nvidia.com>
2024-07-08 19:31:26 +0000

    Add backward function

--- dreamplace/ops/net_crossing/src/net_crossing.cpp
+++ dreamplace/ops/net_crossing/src/net_crossing.cpp
@@ -145,8 +145,10 @@ void computeNetCrossingLauncher(const T* x, const T* y, const int* flat_netpin,
 /// the number of pins in each net, the length of IA is number of nets + 1
 /// @param net_mask an array to record whether compute the netcrossing for a net or
 /// not
-at::Tensor net_crossing_forward(at::Tensor pos, at::Tensor flat_netpin,
-                               at::Tensor netpin_start, at::Tensor net_mask) {
+std::vector<at::Tensor> net_crossing_forward(at::Tensor pos, at::Tensor flat_netpin,
+                               at::Tensor netpin_start, at::Tensor net_mask,
+                               at::Tensor lambda, at::Tensor mu, at::Tensor sigma // scalar Tensors
+) {
   CHECK_FLAT_CPU(pos);
   CHECK_EVEN(pos);
   CHECK_CONTIGUOUS(pos);
@@ -159,21 +161,47 @@ at::Tensor net_crossing_forward(at::Tensor pos, at::Tensor flat_netpin,
 
 
   int num_nets = netpin_start.numel() - 1;
+  int num_pins = pos.numel() / 2;
+
   at::Tensor net_crossing = at::zeros(num_nets, pos.options());
+  at::Tensor grad_intermediate = at::zeros_like(pos);
 
   DREAMPLACE_DISPATCH_FLOATING_TYPES(pos, "computeNetCrossingLauncher", [&] {
     computeNetCrossingLauncher<scalar_t>(
         DREAMPLACE_TENSOR_DATA_PTR(pos, scalar_t),
-        DREAMPLACE_TENSOR_DATA_PTR(pos, scalar_t) + pos.numel() / 2,
+        DREAMPLACE_TENSOR_DATA_PTR(pos, scalar_t) + num_pins,
         DREAMPLACE_TENSOR_DATA_PTR(flat_netpin, int),
         DREAMPLACE_TENSOR_DATA_PTR(netpin_start, int),
-        DREAMPLACE_TENSOR_DATA_PTR(net_mask, unsigned char), num_nets,
-        at::get_num_threads(), DREAMPLACE_TENSOR_DATA_PTR(net_crossing, scalar_t));
+        DREAMPLACE_TENSOR_DATA_PTR(net_mask, unsigned char), 
+        num_nets,
+        DREAMPLACE_TENSOR_DATA_PTR(net_crossing, scalar_t),
+        DREAMPLACE_TENSOR_DATA_PTR(lambda, scalar_t),
+        DREAMPLACE_TENSOR_DATA_PTR(mu, scalar_t),
+        DREAMPLACE_TENSOR_DATA_PTR(sigma, scalar_t),
+        DREAMPLACE_TENSOR_DATA_PTR(grad_intermediate, scalar_t),
+        DREAMPLACE_TENSOR_DATA_PTR(grad_intermediate, scalar_t) + num_pins,
+        at::get_num_threads()
   });
 
-  return net_crossing;
+  return {net_crossing.sum(), grad_intermediate};
 }
 
+
+at::Tensor net_crossing_backward(
+  at::Tensor grad_pos, at::Tensor pos, at::Tensor grad_intermediate
+) {
+
+  CHECK_FLAT_CPU(pos);
+  CHECK_EVEN(pos);
+  CHECK_CONTIGUOUS(pos);
+  CHECK_FLAT_CPU(grad_intermediate);
+  CHECK_EVEN(grad_intermediate);
+  CHECK_CONTIGUOUS(grad_intermediate);
+
+  at::Tensor grad_out = grad_intermediate.mul_(grad_pos);
+
+  return grad_out;
+}
 
 
 DREAMPLACE_END_NAMESPACE
@@ -181,4 +209,6 @@ DREAMPLACE_END_NAMESPACE
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
   m.def("forward", &DREAMPLACE_NAMESPACE::net_crossing_forward,
         "NetCrossing forward");
+  m.def("backward", &DREAMPLACE_NAMESPACE::net_crossing_backward,
+        "NetCrossing backward");
 }
