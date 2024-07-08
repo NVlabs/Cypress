1824c0ef2267673ae489261f121bfec70df1af69
Niansong Zhang <niansongz@nvidia.com>
2024-07-08 19:18:35 +0000

    Add gradient computation

--- dreamplace/ops/net_crossing/src/net_crossing.cpp
+++ dreamplace/ops/net_crossing/src/net_crossing.cpp
@@ -14,6 +14,7 @@ template <typename T>
 void computeNetCrossingLauncher(const T* x, const T* y, const int* flat_netpin,
                                const int* netpin_start, const unsigned char* net_mask,
                                int num_nets, T* net_crossing,
+                               T lambda, T mu, T sigma,
                                T *grad_intermediate_x, T *grad_intermediate_y,
                                int num_threads) {
 #pragma omp parallel for num_threads(num_threads)
@@ -45,23 +46,89 @@ void computeNetCrossingLauncher(const T* x, const T* y, const int* flat_netpin,
           T y4 = y[net_j_sink_pin_id];
 
            // Bezier curve intersection
-          T r = (x1 - x3) * (y3 - y4) - (y1 - y3) * (x3 - x4) / ((x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4));
+          T t = (x1 - x3) * (y3 - y4) - (y1 - y3) * (x3 - x4) / ((x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4));
           T u = (x1 - x2) * (y1 - y3) - (y1 - y2) * (x1 - x3) / ((x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4));
           u = -u;
 
           // Bell function
-          // TODO(Niansong): parameterize
+          // lambda = 2, mu = 2, sigma = 1
           auto bell = [](T x) {
             if std::abs(x) <= 0.5 {
-              return 1 - 2 * x * x;
+              return 1 - lambda * x * x;
             } else if std::abs(x) <= 1 {
-              return 2 * (1 - std::abs(x)) * (1 - std::abs(x));
+              return mu * (sigma - std::abs(x)) * (sigma - std::abs(x));
             } else {
               return 0.0;
             }
           };
 
-          net_crossing[i] += bell(r - 0.5) * bell(u - 0.5);
+          net_crossing[i] += bell(t - 0.5) * bell(u - 0.5);
+
+          // compute gradient
+          auto bell_gradient = [](T x) {
+            if std::abs(x) <= 0.5 {
+              return -2 * lambda * x;
+            } else if std::abs(x) <= 1 {
+              return 2 * mu * x * (std::abs(x) - sigma) / std::abs(x);
+            } else {
+              return 0.0;
+            }
+          };
+
+          T dt_dx1 = ((y3 - y4) * (x4 * (y3 - y2) + x3 * (y2 - y4) + x2 * (y4 - y3))) / std::pow(-x3 * (y1 - y2) - x4 * (y2 - y1) + (x1 - x2) * (y3 - y4), 2);
+          T dt_dy1 = ((x3 - x4) * (-x4 * (y3 - y2) - x3 * (y2 - y4) + x2 * (y3 - y4))) / std:pow((x1 - x2) * (y3 - y4) - (x3 - x4) * (y1 - y2), 2);
+          T dt_dx2 = -((y4 - y3) * ((x1 - x3) * (y3 - y4) - (x3 - x4) * (y1 - y3))) / std::pow((x1 - x2) * (y3 - y4) - (x3 - x4) * (y1 - y2), 2);
+          T dt_dy2 = -((x3 - x4) * ((x1 - x3) * (y3 - y4) - (x3 - x4) * (y1 - y3))) / std::pow((x1 - x2) * (y3 - y4) - (x3 - x4) * (y1 - y2), 2);
+          T dt_dx3 = ((y4 - y3) * (-x4 * (y2 - y1) - x2 * (y1 - y4) + x1 * (y2 - y4))) / std::pow((x1 - x2) * (y3 - y4) - (x3 - x4) * (y1 - y2), 2);
+          T dt_dy3 = ((x3 - x4) * (x4 * (y1 - y2) + x1 * (y2 - y4) + x2 * (y4 - y1))) / std::pow(-x3 * (y1 - y2) - x4 * (y2 - y1) + (x1 - x2) * (y3 - y4), 2);
+          T dt_dx4 = ((y3 - y4) * (x3 * (y1 - y2) + x1 * (y2 - y3) + x2 * (y3 - y1))) / std::pow((x3 - x4) * (y1 - y2) - (x1 - x2) * (y3 - y4), 2);
+          T dt_dy4 = ((x4 - x3) * (-x3 * (y2 - y1) - x2 * (y1 - y3) + x1 * (y2 - y3)))/ std::pow((x1 - x2) * (y3 - y4) - (x3 - x4) * (y1 - y2), 2);
+
+          T du_dx1 = ((y2 - y1) * (-x4 * (y3 - y2) - x3 * (y2 - y4) + x2 * (y3 - y4))) / std::pow((x1 - x2) * (y3 - y4) - (x3 - x4) * (y1 - y2), 2);
+          T du_dy1 = ((x1 - x2) * (x4 * (y2 - y3) + x2 * (y3 - y4) + x3 * (y4 - y2))) / std::pow(-x3 * (y1 - y2) - x4 * (y2 - y1) + (x1 - x2) * (y3 - y4), 2);
+          T du_dx2 = -((y1 - y2) * (x4 * (y3 - y1) + x3 * (y1 - y4) + x1 * (y4 - y3))) / std::pow(-x3 * (y1 - y2) - x4 * (y2 - y1) + (x1 - x2) * (y3 - y4), 2);
+          T du_dy2 = -((x1 - x2) * (x4 * (y1 - y3) + x1 * (y3 - y4) + x3 * (y4 - y1))) / std::pow((x3 - x4) * (y1 - y2) - (x1 - x2) * (y3 - y4), 2);
+          T du_dx3 = ((y1 - y2) * (x4 * (y2 - y1) + x2 * (y1 - y4) + x1 * (y4 - y2))) / std::pow(-x3 * (y1 - y2) - x4 * (y2 - y1) + (x1 - x2) * (y3 - y4), 2);
+          T du_dy3 = ((x1 - x2) * (x4 * (y1 - y2) + x1 * (y2 - y4) + x2 * (y4 - y1))) / std::pow(-x3 * (y1 - y2) - x4 * (y2 - y1) + (x1 - x2) * (y3 - y4), 2);
+          T du_dx4 = ((y1 - y2) * ((x1 - x2) (y1 - y3) - (x1 - x3) (y1 - y2))) / std::pow((x1 - x2) * (y3 - y4) - (x3 - x4) * (y1 - y2), 2);
+          T du_dy4 = ((x2 - x1) * ((x1 - x2) (y1 - y3) - (x1 - x3) (y1 - y2))) / std::pow((x1 - x2) * (y3 - y4) - (x3 - x4) * (y1 - y2), 2);
+
+          // NC = f * g
+          T df_dx1 = bell_gradient(t) * dt_dx1;
+          T df_dy1 = bell_gradient(t) * dt_dy1;
+          T df_dx2 = bell_gradient(t) * dt_dx2;
+          T df_dy2 = bell_gradient(t) * dt_dy2;
+          T df_dx3 = bell_gradient(t) * dt_dx3;
+          T df_dy3 = bell_gradient(t) * dt_dy3;
+          T df_dx4 = bell_gradient(t) * dt_dx4;
+          T df_dy4 = bell_gradient(t) * dt_dy4;
+
+          T dg_dx1 = bell_gradient(u) * du_dx1;
+          T dg_dy1 = bell_gradient(u) * du_dy1;
+          T dg_dx2 = bell_gradient(u) * du_dx2;
+          T dg_dy2 = bell_gradient(u) * du_dy2;
+          T dg_dx3 = bell_gradient(u) * du_dx3;
+          T dg_dy3 = bell_gradient(u) * du_dy3;
+          T dg_dx4 = bell_gradient(u) * du_dx4;
+          T dg_dy4 = bell_gradient(u) * du_dy4;
+
+          T dx1 = df_dx1 * bell(u) + bell(t) * dg_dx1;
+          T dy1 = df_dy1 * bell(u) + bell(t) * dg_dy1;
+          T dx2 = df_dx2 * bell(u) + bell(t) * dg_dx2;
+          T dy2 = df_dy2 * bell(u) + bell(t) * dg_dy2;
+          T dx3 = df_dx3 * bell(u) + bell(t) * dg_dx3;
+          T dy3 = df_dy3 * bell(u) + bell(t) * dg_dy3;
+          T dx4 = df_dx4 * bell(u) + bell(t) * dg_dx4;
+          T dy4 = df_dy4 * bell(u) + bell(t) * dg_dy4;
+          
+          grad_intermediate_x[net_i_src_pin_id] = dx1;
+          grad_intermediate_y[net_i_src_pin_id] = dy1;
+          grad_intermediate_x[net_i_sink_pin_id] = dx2;
+          grad_intermediate_y[net_i_sink_pin_id] = dy2;
+          grad_intermediate_x[net_j_src_pin_id] = dx3;
+          grad_intermediate_y[net_j_src_pin_id] = dy3;
+          grad_intermediate_x[net_j_sink_pin_id] = dx4;
+          grad_intermediate_y[net_j_sink_pin_id] = dy4;
         }
       }
     }
