eea425d69b89815fa89ac391a3bce4303c000cc9
Niansong Zhang <niansongz@nvidia.com>
2024-07-08 04:19:20 +0000

    Add source-sink net crossing

--- dreamplace/ops/net_crossing/src/net_crossing.cpp
+++ dreamplace/ops/net_crossing/src/net_crossing.cpp
@@ -11,68 +11,59 @@ DREAMPLACE_BEGIN_NAMESPACE
 
 
 template <typename T>
-int computeNetCrossingLauncher(const T* x, const T* y, const int* flat_netpin,
+void computeNetCrossingLauncher(const T* x, const T* y, const int* flat_netpin,
                                const int* netpin_start, const unsigned char* net_mask,
-                               int num_nets, int num_threads, T* net_crossing){
+                               int num_nets, T* net_crossing,
+                               T *grad_intermediate_x, T *grad_intermediate_y,
+                               int num_threads) {
 #pragma omp parallel for num_threads(num_threads)
   for (int i = 0; i < num_nets; ++i) {
     for (int j = 0; j < num_nets; ++j) {
       if (i == j) continue;
       if (!net_mask[i] || !net_mask[j]) continue;
 
-      // find the bounding box of pins in net i and net j
-      T i_max_x = -std::numeric_limits<T>::max();
-      T i_min_x = std::numeric_limits<T>::max();
-      T i_max_y = -std::numeric_limits<T>::max();
-      T i_min_y = std::numeric_limits<T>::max();
-      T j_max_x = -std::numeric_limits<T>::max();
-      T j_min_x = std::numeric_limits<T>::max();
-      T j_max_y = -std::numeric_limits<T>::max();
-      T j_min_y = std::numeric_limits<T>::max();
-      for (int k = netpin_start[i]; k < netpin_start[i + 1]; ++k) {
-        int pin_id_i = flat_netpin[k];
-        i_max_x = std::max(i_max_x, x[pin_id_i]);
-        i_min_x = std::min(i_min_x, x[pin_id_i]);
-        i_max_y = std::max(i_max_y, y[pin_id_i]);
-        i_min_y = std::min(i_min_y, y[pin_id_i]);
+      // TODO(Niansong): if j > i directly read from previous computation results
+
+      // skip if net i/j has only one pin
+      if (netpin_start[i + 1] - netpin_start[i] <= 1 || netpin_start[j + 1] - netpin_start[j] <= 1) {
+        continue;
       }
-      for (int k = netpin_start[j]; k < netpin_start[j + 1]; ++k) {
-        int pin_id_j = flat_netpin[k];
-        j_max_x = std::max(j_max_x, x[pin_id_j]);
-        j_min_x = std::min(j_min_x, x[pin_id_j]);
-        j_max_y = std::max(j_max_y, y[pin_id_j]);
-        j_min_y = std::min(j_min_y, y[pin_id_j]);
-      }
+      for (int net_i_sink_pin_idx = netpin_start[i] + 1; net_i_sink_pin_idx < netpin_start[i + 1]; ++net_i_sink_pin_idx) {
+        int net_i_src_pin_id = flat_netpin[netpin_start[i]];
+        int net_i_sink_pin_id = flat_netpin[net_i_sink_pin_idx];
+        for (int net_j_sink_pin_idx = netpin_start[j] + 1; net_j_sink_pin_idx < netpin_start[j + 1]; ++net_j_sink_pin_idx) {
+          int net_j_src_pin_id = flat_netpin[netpin_start[j]];
+          int net_j_sink_pin_id = flat_netpin[net_j_sink_pin_idx];
+          
+          T x1 = x[net_i_src_pin_id];
+          T y1 = y[net_i_src_pin_id];
+          T x2 = x[net_i_sink_pin_id];
+          T y2 = y[net_i_sink_pin_id];
+          T x3 = x[net_j_src_pin_id];
+          T y3 = y[net_j_src_pin_id];
+          T x4 = x[net_j_sink_pin_id];
+          T y4 = y[net_j_sink_pin_id];
 
-      // net i: (i_min_x, i_min_y) -> (i_max_x, i_max_y)
-      // net j: (j_min_x, j_min_y) -> (j_max_x, j_max_y)
-      T x1 = i_min_x;
-      T y1 = i_min_y;
-      T x2 = i_max_x;
-      T y2 = i_max_y;
-      T x3 = j_min_x;
-      T y3 = j_min_y;
-      T x4 = j_max_x;
-      T y4 = j_max_y;
+           // Bezier curve intersection
+          T r = (x1 - x3) * (y3 - y4) - (y1 - y3) * (x3 - x4) / ((x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4));
+          T u = (x1 - x2) * (y1 - y3) - (y1 - y2) * (x1 - x3) / ((x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4));
+          u = -u;
 
-      // Bezier curve intersection
-      T r = (x1 - x3) * (y3 - y4) - (y1 - y3) * (x3 - x4) / ((x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4));
-      T u = (x1 - x2) * (y1 - y3) - (y1 - y2) * (x1 - x3) / ((x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4));
-      u = -u;
+          // Bell function
+          // TODO(Niansong): parameterize
+          auto bell = [](T x) {
+            if std::abs(x) <= 0.5 {
+              return 1 - 2 * x * x;
+            } else if std::abs(x) <= 1 {
+              return 2 * (1 - std::abs(x)) * (1 - std::abs(x));
+            } else {
+              return 0.0;
+            }
+          };
 
-      // Bell function
-      // TODO(Niansong): parameterize
-      auto bell = [](T x) {
-        if std::abs(x) <= 0.5 {
-          return 0.75 - x * x;
-        } else if std::abs(x) <= 1 {
-          return 0.5 * (1.5 - std::abs(x)) * (1.5 - std::abs(x));
-        } else {
-          return 0.0;
+          net_crossing[i] += bell(r - 0.5) * bell(u - 0.5);
         }
-      };
-
-      net_crossing[i] += bell(r - 0.5) * bell(u - 0.5);
+      }
     }
   }
 
