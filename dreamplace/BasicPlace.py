Split gradients, fix minor errors

dreamplace/BasicPlace.py
@@ -637,9 +637,10 @@ class BasicPlace(nn.Module):
             flat_node2pin_map=data_collections.flat_node2pin_map,
             flat_node2pin_start_map=data_collections.flat_node2pin_start_map,
             num_physical_nodes=placedb.num_physical_nodes,
-            h=data_collections.node_size_x,
-            w=data_collections.node_size_y,
+            h=data_collections.node_size_y,
+            w=data_collections.node_size_x,
             algorithm="node-by-node",
+            orient_logits=data_collections.orient_logits,
         )
 
     def build_move_boundary(self, params, placedb, data_collections, device):

dreamplace/PlaceDB.py
@@ -1010,13 +1010,13 @@ class PlaceDB(object):
         # first step we only use four orientations: N, S, W, E
         # we will add the other four orientations later if we want to let the tool decide
         # side of the components too
-        self.orient_logits = np.zeros((self.num_physical_nodes, 4), dtype=np.int32)
+        self.orient_logits = np.zeros((self.num_physical_nodes, 4), dtype=np.float32)
         for i in range(self.num_physical_nodes):
             node_orient_i = str(self.node_orient[i])
             if 'N' in node_orient_i:
                 self.orient_logits[i, 0] = 1
             elif 'S' in node_orient_i:
-                self.orientorient_logits_one_hot[i, 1] = 1
+                self.orient_logits[i, 1] = 1
             elif 'W' in node_orient_i:
                 self.orient_logits[i, 2] = 1
             elif 'E' in node_orient_i:

dreamplace/ops/pin_pos/pin_pos.py
@@ -59,7 +59,11 @@ class PinPosFunction(Function):
                       ctx.flat_node2pin_map, ctx.flat_node2pin_start_map,
                       ctx.h, ctx.w,
                       ctx.num_physical_nodes)
-        return output, None, None, None, None, None, None
+        # split pos and theta gradients
+        num_nodes = ctx.pos.numel() // 2
+        grad_pos = output[:num_nodes * 2]
+        grad_theta = output[num_nodes * 2:]
+        return grad_pos, None, None, grad_theta, None, None, None, None, None, None
 
 
 class PinPosSegmentFunction(Function):
@@ -147,11 +151,11 @@ class PinPos(nn.Module):
         """
         assert pos.numel() % 2 == 0
         num_nodes = pos.numel() // 2
-        if self.theta is not None:
+        if self.orient_logits is not None:
             # rotation is enabled
             y = torch.nn.functional.gumbel_softmax(self.orient_logits, tau=1.0, hard=True)
-            index_tensor = torch.arange(4).unsqueeze(0).expand_as(y)
-            choices = torch.sum(self.y * index_tensor, dim=1)
+            index_tensor = torch.arange(4).unsqueeze(0).expand_as(y).to(y.device)
+            choices = torch.sum(y * index_tensor, dim=1)
             self.theta = choices * math.pi / 2
         else:
             # initialize theta to 0