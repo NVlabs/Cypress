Fix issues with rotation optimizer

dreamplace/NonLinearPlace.py
@@ -133,7 +133,13 @@ class NonLinearPlace(BasicPlace.BasicPlace):
                     assert 0, "unknown optimizer %s" % (optimizer_name)
 
                 if params.enable_rotation:
-                    rot_optimizer = torch.optim.SGD(orient_logits, lr=0.0001, momentum=0.9, nesterov=True)
+                    rot_optimizer = torch.optim.SGD(orient_logits, lr=0)
+                    # rot_optimizer = NesterovAcceleratedGradientOptimizer.NesterovAcceleratedGradientOptimizer(
+                    #     orient_logits,
+                    #     lr=0,
+                    #     obj_and_grad_fn=model.obj_and_grad_fn,
+                    #     constraint_fn=self.op_collections.move_boundary_op,
+                    # )
 
                 logging.info("use %s optimizer" % (optimizer_name))
                 model.train()
@@ -393,19 +399,21 @@ class NonLinearPlace(BasicPlace.BasicPlace):
                     update_orient_cond = (
                         iteration > 1000 and iteration % 100 == 0
                     )
-                    update_orient_cond = True
                     
                     if update_orient_cond is True:
                         if params.enable_rotation:
                             best_wl, best_nc = model.wirelength.data, model.net_crossing.data
-                            for _ in range(10):
+                            for _ in range(20):
                                 model.op_collections.pin_pos_op.use_best_theta = False
                                 model.freeze_pos = True
                                 obj_rot, grad_rot = model.obj_and_grad_fn(pos, orient_logits=self.orient_logits)
                                 rot_optimizer.step()
-                                if model.wirelength.data < best_wl or model.net_crossing.data < best_nc:
+                                # if model.wirelength.data < best_wl or model.net_crossing.data < best_nc:
+                                if model.wirelength.data < best_wl:
                                     self.update_best_theta()
                                     best_wl, best_nc = model.wirelength.data, model.net_crossing.data
+                                model.freeze_pos = False
+                                model.op_collections.pin_pos_op.use_best_theta = True
                     else:
                         model.op_collections.pin_pos_op.use_best_theta = True
                         model.freeze_pos = False

dreamplace/PlaceDB.py
@@ -1010,23 +1010,23 @@ class PlaceDB(object):
         # we will add the other four orientations later if we want to let the tool decide
         # side of the components too
         self.orient_logits = np.zeros((self.num_physical_nodes, 4), dtype=np.float32)
-        for i in range(self.num_physical_nodes):
+        # for i in range(self.num_physical_nodes):
             # if enable_rotation:
             #     node_orient_i = str(self.node_orient[i])
             # else:
             #     node_orient_i = 'N'
             # Always set initial orientation to N is better
-            node_orient_i = 'N'
-            if 'N' in node_orient_i:
-                self.orient_logits[i, 0] = 1
-            elif 'W' in node_orient_i:
-                self.orient_logits[i, 1] = 1
-            elif 'S' in node_orient_i:
-                self.orient_logits[i, 2] = 1
-            elif 'E' in node_orient_i:
-                self.orient_logits[i, 3] = 1
-            else:
-                logging.error(f"Unknown orientation {self.node_orient[i]} for node {self.node_names[i]}")
+            # node_orient_i = 'N'
+            # if 'N' in node_orient_i:
+            #     self.orient_logits[i, 0] = 1
+            # elif 'W' in node_orient_i:
+            #     self.orient_logits[i, 1] = 1
+            # elif 'S' in node_orient_i:
+            #     self.orient_logits[i, 2] = 1
+            # elif 'E' in node_orient_i:
+            #     self.orient_logits[i, 3] = 1
+            # else:
+            #     logging.error(f"Unknown orientation {self.node_orient[i]} for node {self.node_names[i]}")
 
     def initialize(self, params):
         """

dreamplace/ops/pin_pos/pin_pos.py
@@ -154,7 +154,7 @@ class PinPos(nn.Module):
         assert pos.numel() % 2 == 0
         num_nodes = pos.numel() // 2
 
-        if self.use_best_theta:
+        if self.use_best_theta or self.orient_logits is None:
             self.theta = self.best_theta
         else:
             # rotation is enabled