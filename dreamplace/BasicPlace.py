Fix issues with gumbel-softmax initialization

dreamplace/BasicPlace.py
@@ -98,12 +98,8 @@ class PlaceDataCollection(object):
 
         with torch.no_grad():
             # other tensors required to build ops
-            if orient_logits is not None:
-                self.best_theta = torch.zeros(self.orient_logits.size(0), dtype=self.orient_logits.dtype, device=device)
-                self.best_orient_choice = torch.zeros(self.orient_logits.size(0), dtype=torch.int32, device=device)
-            else:
-                self.best_theta = torch.tensor(placedb.orient_logits, dtype=self.pos[0].dtype, device=device)
-                self.best_orient_choice = torch.zeros(placedb.orient_logits.shape[0], dtype=torch.int32, device=device)
+            self.best_theta = torch.zeros(placedb.orient_logits.shape[0], dtype=self.pos[0].dtype, device=device)
+            self.best_orient_choice = torch.zeros(placedb.orient_logits.shape[0], dtype=torch.int32, device=device)
             self.node_size_x = torch.from_numpy(placedb.node_size_x).to(device)
             self.node_size_y = torch.from_numpy(placedb.node_size_y).to(device)
             # original node size for legalization, since they will be adjusted in global placement

dreamplace/NonLinearPlace.py
@@ -133,7 +133,8 @@ class NonLinearPlace(BasicPlace.BasicPlace):
                     assert 0, "unknown optimizer %s" % (optimizer_name)
 
                 if params.enable_rotation:
-                    rot_optimizer = torch.optim.SGD(orient_logits, lr=0)
+                    # rot_optimizer = torch.optim.SGD(orient_logits, lr=0)
+                    rot_optimizer = torch.optim.Adam(orient_logits, lr=0)
                     # rot_optimizer = NesterovAcceleratedGradientOptimizer.NesterovAcceleratedGradientOptimizer(
                     #     orient_logits,
                     #     lr=0,
@@ -397,23 +398,28 @@ class NonLinearPlace(BasicPlace.BasicPlace):
                     optimizer.zero_grad()
 
                     update_orient_cond = (
-                        iteration > 1000 and iteration % 100 == 0
+                        iteration >= 1000 and iteration % 100 == 0
                     )
                     
-                    if update_orient_cond is True:
-                        if params.enable_rotation:
-                            best_wl, best_nc = model.wirelength.data, model.net_crossing.data
-                            for _ in range(20):
-                                model.op_collections.pin_pos_op.use_best_theta = False
-                                model.freeze_pos = True
-                                obj_rot, grad_rot = model.obj_and_grad_fn(pos, orient_logits=self.orient_logits)
-                                rot_optimizer.step()
-                                # if model.wirelength.data < best_wl or model.net_crossing.data < best_nc:
-                                if model.wirelength.data < best_wl:
-                                    self.update_best_theta()
-                                    best_wl, best_nc = model.wirelength.data, model.net_crossing.data
-                                model.freeze_pos = False
-                                model.op_collections.pin_pos_op.use_best_theta = True
+                    if params.enable_rotation and update_orient_cond:
+                        best_wl, best_nc = model.wirelength.data, model.net_crossing.data
+                        for _ in range(100):
+                            model.op_collections.pin_pos_op.use_best_theta = False
+                            model.freeze_pos = True
+                            obj_rot, grad_rot = model.obj_and_grad_fn(pos, orient_logits=self.orient_logits)
+                            # with torch.no_grad():
+                                # grad_rot[1:,:] = 0
+                                # grad_rot = 0
+                            rot_optimizer.step()
+                            # with torch.no_grad():
+                                # self.orient_logits -= 1e-5 * grad_rot
+                            if model.wirelength.data < best_wl or model.net_crossing.data < best_nc:
+                            # if model.wirelength.data < best_wl:
+                                logging.info("update best theta")
+                                self.update_best_theta()
+                                best_wl, best_nc = model.wirelength.data, model.net_crossing.data
+                            model.freeze_pos = False
+                            model.op_collections.pin_pos_op.use_best_theta = True
                     else:
                         model.op_collections.pin_pos_op.use_best_theta = True
                         model.freeze_pos = False

dreamplace/PlaceObj.py
@@ -405,6 +405,7 @@ class PlaceObj(nn.Module):
 
         self.wirelength = self.op_collections.wirelength_op(pos)
         result = torch.add(result, self.wirelength)
+        
 
         if self.params.net_crossing_flag:
             self.net_crossing = self.op_collections.net_crossing_op(pos)
@@ -420,8 +421,8 @@ class PlaceObj(nn.Module):
                 nc_grad_norm = nc_grad.norm(p=1)
                 nc_grad_scale = wl_grad_norm / nc_grad_norm
                 logging.info(f"wirelength grad norm = {wl_grad_norm}, net crossing grad norm = {nc_grad_norm}")
-            if not math.isnan(nc_grad_norm):
-                result = torch.add(result, self.net_crossing, alpha=nc_grad_scale * self.net_crossing_weight)
+            # if not math.isnan(nc_grad_norm):
+            #     result = torch.add(result, self.net_crossing, alpha=nc_grad_scale * self.net_crossing_weight)
 
         if orient_logits is not None:
             return result