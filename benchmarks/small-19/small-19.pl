d8e201549c9685a9624960b7ccad720cfc12d105
Niansong Zhang <niansongz@nvidia.com>
2024-10-29 00:37:52 +0000

    Add small benchmarks

---
+++ benchmarks/small-19/small-19.pl
@@ -0,0 +1,34 @@
+UCLA pl 1.0
+
+
+# small-19.pl generated on 2024-10-27T184702 using /home/scratch.niansongz_research/pcb-util/bookshelf_converter.py 1.0
+
+C1036 75 45 : FN 
+C1032 101 45 : FN 
+C2378 18 23 : N 
+C2379 46 26 : N 
+C2059 63 72 : FN 
+C2033 39 69 : N 
+C2058 74 29 : N 
+C2057 111 101 : N 
+C2056 109 29 : N 
+C2032 47 83 : N 
+C2055 63 64 : FN 
+C1983 109 53 : FN 
+C2054 109 61 : FN 
+C2053 108 68 : FN 
+C2469 22 40 : N 
+C234 0 27 : N 
+C1430 69 108 : N 
+C1429 126 101 : FN 
+C1428 107 0 : FN 
+C1427 99 7 : FN 
+C1638 47 69 : N 
+C1426 48 54 : N 
+C1947 109 45 : FN 
+C1957 55 77 : N 
+C1425 83 64 : FN 
+C1424 55 94 : FN 
+C1423 55 94 : N 
+C2158 69 45 : N 
+C2196 8 64 : N 
